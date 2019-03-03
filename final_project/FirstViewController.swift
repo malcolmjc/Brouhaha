6b4016a27784712afebec684594470e81ad93425
liblabs-mac <liblabs-mac@35114b45-222207.ad.calpoly.edu>
2019-03-02 16:59:44 -0800

    Initial Commit

---
+++ final_project/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  final_project
+//
+//  Created by liblabs-mac on 3/2/19.
+//  Copyright © 2019 liblabs-mac. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ final_project/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  final_project
+//
+//  Created by liblabs-mac on 3/2/19.
+//  Copyright © 2019 liblabs-mac. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
